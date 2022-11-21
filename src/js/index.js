aadd2f053b53e38ed0f0c6c4033080e5b34b1cc4
ThankGod Richard <thankgod@lemonade.finance>
2022-11-21 23:45:20 +0100

    font awesome added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
+/**
+ * @author ThankGod Richard
+ * @usecase Importin Bootstrap
+ */
 import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../../node_modules/bootstrap/dist/js/bootstrap.js"
+
+
+/**
+ * @author ThankGod Richard
+ * @usecase Importin FontAwesom
+ */
+ import '@fortawesome/fontawesome-free/js/fontawesome'
+ import '@fortawesome/fontawesome-free/js/solid'
+ import '@fortawesome/fontawesome-free/js/regular'
+ import '@fortawesome/fontawesome-free/js/brands'
