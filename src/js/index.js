bf659366906d6ccc41abe8869990625c171a40a6
momotsow <kgomotsomkansi@gmail.com>
2022-11-22 20:53:33 +0200

    Add header and footer

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,5 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+import '../css/style.css';
